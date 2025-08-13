af9caf4bba2223527cb787cad024ab5a25748b0a
coc-webmaster <webmaster@chipolsonconsulting.com>
2025-08-13 16:03:32 -0600

    favicon update

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/css": "css" });
   eleventyConfig.addPassthroughCopy({ "src/images": "images" });
   eleventyConfig.addPassthroughCopy({ "src/js": "js" });
+  eleventyConfig.addPassthroughCopy("src/favicons");
+
 
   return {
     dir: {
